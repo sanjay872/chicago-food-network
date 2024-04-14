33919f2caef4812395269dd4b6d55aa0b91c4195
sanjay <rockysanjay287@gmail.com>
2024-04-14 15:52:26 -0500

    backend setup

---
+++ food-network-backend/src/main/java/chicago/foodnetworkbackend/FoodNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package chicago.foodnetworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodNetworkBackendApplication.class, args);
+	}
+
+}
---
+++ food-network-backend/src/test/java/chicago/foodnetworkbackend/FoodNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package chicago.foodnetworkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
